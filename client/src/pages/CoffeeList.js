230dcc10a9d9f6f78914b183cf7daf8c011a55e8
harmanpreet03 <harmanpreetghotra17@gmail.com>
2023-10-18 15:09:58 -0400

    css edited

--- client/src/pages/CoffeeList.js
+++ client/src/pages/CoffeeList.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 const CoffeeList = ({ coffees }) => {
+  const itemsPerRow = 5; // Number of items per row
+  const groupedCoffees = [];
+  for (let i = 0; i < coffees.length; i += itemsPerRow) {
+    groupedCoffees.push(coffees.slice(i, i + itemsPerRow));
+  }
+
   return (
     <div className="coffee-list">
-      {coffees.map((coffee) => (
-        <div key={coffee.id} className="coffee-item">
-          <img
-            src={coffee.image_url}
-            alt={coffee.name}
-            className="coffee-image"
-          />
-          <p>{coffee.name}</p>
+      {groupedCoffees.map((row, index) => (
+        <div key={index} className="coffee-row">
+          {row.map((coffee) => (
+            <div key={coffee.id} className="coffee-item">
+              <img
+                src={coffee.image_url}
+                alt={coffee.name}
+                className="coffee-image"
+              />
+              <p>{coffee.name}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
