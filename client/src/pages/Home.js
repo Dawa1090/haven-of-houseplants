58c626654cad079efef7197f34da4bb47abb9072
harmanpreet03 <harmanpreetghotra17@gmail.com>
2023-10-18 16:02:54 -0400

    login logout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Login from "./Login";
 import Signup from "./Signup";
 import UserDetails from "./UserDetails";
 import './styles.css'; 
 
 function Home({ currentUser, attemptLogin, attemptSignup, logout }) {
-    console.log(currentUser)
+    console.log(currentUser);
+
     return (
         <div className='background-image'>
             <div className='headerContainer'>
                 <h1 className='titleContainer'>Let's Caffeine</h1>
-                {currentUser.username ? (
+                {currentUser && currentUser.username ? (
                     <div>
                         <UserDetails currentUser={currentUser} logout={logout} />
                         <Link to='/coffee'>
@@ -22,7 +23,6 @@ function Home({ currentUser, attemptLogin, attemptSignup, logout }) {
                     <div>
                         <Login attemptLogin={attemptLogin}  />
                         <Signup attemptSignup={attemptSignup} />
-                        
                     </div>
                 )}
             </div>
@@ -31,5 +31,3 @@ function Home({ currentUser, attemptLogin, attemptSignup, logout }) {
 }
 
 export default Home;
-
-
