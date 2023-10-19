e5eef78705b88582a7a2e69ac0cc067d0e10e890
harmanpreet03 <harmanpreetghotra17@gmail.com>
2023-10-19 15:03:00 -0400

    done

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,30 +1,29 @@
-import React from 'react'
-
-
+import React from 'react';
+import './About.css';
 
 function About() {
   return (
     <div className='quote'>
       <h1>"Coffee Chronicles: A Cup for Every Occasion"</h1>
-      <p className='about-container'>
+      <div className='about-container'>
         <p>
-        In the world of coffee, there's a brew for every mood and occasion. It's like having that perfect friend who always knows how to fit in. For early risers, coffee is the comforting hug that kickstarts their day with promises of productivity. To the night owls, it's the muse that fuels creativity in the silent hours. At the office, it's the reliable coworker that offers a brief escape from the daily grind. Creative souls see coffee as an artistic muse, with each sip igniting the imagination. It's the social connector, bringing people together over cappuccinos, and an adventurous exploration for those seeking exotic flavors and brews. During tough times, it becomes the comforting companion, providing warmth and solace. For fitness enthusiasts, it's the workout partner, boosting energy and motivation. In this coffee world, each cup is a unique love story, a flavorful and fun addition to life's diverse moments.
+          In the world of coffee, there's a brew for every mood and occasion. It's like having that perfect friend who always knows how to fit in. For early risers, coffee is the comforting hug that kickstarts their day with promises of productivity. To the night owls, it's the muse that fuels creativity in the silent hours. At the office, it's the reliable coworker that offers a brief escape from the daily grind. Creative souls see coffee as an artistic muse, with each sip igniting the imagination. It's the social connector, bringing people together over cappuccinos, and an adventurous exploration for those seeking exotic flavors and brews. During tough times, it becomes the comforting companion, providing warmth and solace. For fitness enthusiasts, it's the workout partner, boosting energy and motivation. In this coffee world, each cup is a unique love story, a flavorful and fun addition to life's diverse moments.
         </p>
-      </p>
+      </div>
       <div className='contact'>
         <h2>Contact Me</h2>
         <p>If you have any questions or would like to collaborate, feel free to reach out to me:</p>
         <ul>
-          <li>Email: tasteofcoffee@email.com</li>
+          <li>Email: <a href='mailto:tasteofcoffee@email.com'>tasteofcoffee@email.com</a></li>
           <li>Phone: +1 (123) 456-7890</li>
-          <li>Social Media: <a>Instagram</a> <a>Facebook</a><a>Twitter</a></li>
+          <li>Social Media: <a href='https://www.instagram.com/yourcoffeehandle' target='_blank'>Instagram</a> <a href='https://www.facebook.com/yourcoffeehandle' target='_blank'>Facebook</a> <a href='https://twitter.com/yourcoffeehandle' target='_blank'>Twitter</a></li>
         </ul>
         <footer>
-        &copy; 2023 Taste of Coffee
-      </footer>
+          &copy; 2023 Taste of Coffee
+        </footer>
       </div>
     </div>
   )
 }
 
-export default About
+export default About;
--- client/src/pages/Coffee.js
+++ client/src/pages/Coffee.js
@@ -1,30 +1,121 @@
-// Coffee.js
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ReviewList from "../ReviewList"; 
+import "./Coffee.css";
 
 const Coffee = () => {
   const [coffees, setCoffees] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const postReview = (coffeeId, text, rating, userId) => {
+    const data = {
+      text: text,
+      rating: rating,
+      user_id: userId,
+      coffee_id: coffeeId,
+    };
+
+    fetch("/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((review) => {
+        console.log("Review posted successfully:", review);
+        
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+       
+      });
+  };
 
   useEffect(() => {
-    
     fetch("/coffees")
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
       .then((data) => {
-        setCoffees(data);
+        const initializedCoffees = data.map((coffee) => ({
+          ...coffee,
+          reviewState: {
+            reviewText: "",
+            reviewRating: 1,
+          },
+        }));
+        setCoffees(initializedCoffees);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="Coffee">
-      <h2></h2>
+      <h2 className="coffee-list">Coffee List</h2>
       <div className="coffee-list">
         {coffees.map((coffee) => (
           <div key={coffee.id} className="coffee-item">
             <img className="coffee-image" src={coffee.image_url} alt={coffee.name} />
             <h3>{coffee.name}</h3>
             <p>Reviews: {coffee.reviews.length}</p>
-            <Link to={`/coffee/${coffee.id}/reviews`}>See Reviews</Link>
+            <input
+              type="text"
+              placeholder="Add your review..."
+              value={coffee.reviewState.reviewText}
+              onChange={(e) => {
+                const updatedCoffees = coffees.map((c) =>
+                  c.id === coffee.id
+                    ? { ...c, reviewState: { ...c.reviewState, reviewText: e.target.value } }
+                    : c
+                );
+                setCoffees(updatedCoffees);
+              }}
+            />
+            <label>
+              Rating:
+              <input
+                type="number"
+                min="1"
+                max="5"
+                value={coffee.reviewState.reviewRating}
+                onChange={(e) => {
+                  const updatedCoffees = coffees.map((c) =>
+                    c.id === coffee.id
+                      ? { ...c, reviewState: { ...c.reviewState, reviewRating: parseInt(e.target.value) } }
+                      : c
+                  );
+                  setCoffees(updatedCoffees);
+                }}
+              />
+            </label>
+            <div className="button-group">
+            <button onClick={() => postReview(coffee.id, coffee.reviewState.reviewText, coffee.reviewState.reviewRating, 1)}>
+              Submit Review
+            </button>
+            <Link to={`/coffee/${coffee.id}/reviews`} className= "heelo">See Reviews</Link>
+          </div>
           </div>
         ))}
       </div>
